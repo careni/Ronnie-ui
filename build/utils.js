00eeeb53b7a3e4062d55c436629dc5713ef98be2
careni <526587423@qq.com>
2017-12-28 12:06:39 +0800

    all

--- build/utils.js
+++ build/utils.js
@@ -54,7 +54,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
